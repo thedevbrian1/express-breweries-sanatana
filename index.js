7dc1b17ace396995b5753617e83dd466c38f8c3c
thedevbrian1 <brayomwas95@gmail.com>
2025-04-25 13:37:27 +0300

    Add contact page

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from "express";
 import nodemailer from "nodemailer";
+import path from "path";
+import { fileURLToPath } from "url";
 
 let app = express();
 
 let port = 3000;
 
+let __fileName = fileURLToPath(import.meta.url);
+let __dirName = path.dirname(__fileName);
+
+// Middleware for accessing request body
 app.use(express.urlencoded({ extended: true }));
+
+// Set up assets folder
 app.use(express.static("public"));
 
+// Set up templating engine
 app.set("views", "./views");
 app.set("view engine", "pug");
 
@@ -55,8 +64,6 @@ app.get("/breweries", async (req, res) => {
   let query = req.query;
   let city = query.by_city;
 
-  console.log({ city });
-
   //   If there is a search query,return filtered results, else return all breweries
 
   // Filter by city
@@ -83,6 +90,47 @@ app.get("/breweries", async (req, res) => {
 
   // console.log({ breweries });
   res.render("breweries", { breweries });
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirName, "public", "contact.html"));
+});
+
+app.post("/contact", (req, res) => {
+  // Get submitted values from the request body
+  let body = req.body;
+
+  let name = body.name;
+  let email = body.email;
+  let message = body.message;
+
+  console.log({ name, email, message });
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "brayomwas95@gmail.com",
+      pass: "ymzm rstj rkmh tivp",
+    },
+  });
+
+  transporter.sendMail(
+    {
+      from: "brayomwas95@gmail.com",
+      to: "mwangib041@gmail.com",
+      subject: "Contact from the breweries website",
+      text: `Name: ${name}, Email: ${email}, Message: ${message}`,
+    },
+    (error, info) => {
+      if (error) {
+        console.log(error);
+      }
+      console.log(`Email sent ${info.response}`);
+    }
+  );
+
+  //   TODO: Build success page
+  res.redirect("/success");
 });
 
 app.listen(port, () => {
